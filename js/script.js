069b0e0afb9601c8dbd5e3a7969d79dbf31688c3
fantasma1234 <guilhermedroni07@gmail.com>
2024-07-12 15:23:56 -0300

    Adicionando o localStorage

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
-let n = 0
-let cont = document.querySelector('#cont')  
+let cont = document.querySelector('#cont')
+cont.innerText = localStorage.getItem("cont") || "0"
+let n = parseInt(cont.innerText)
 function somar() {
     n++
     cont.innerHTML = n
+    Salvar()
 }
 
 function subtrair() {
     n--
     cont.innerHTML = n
+    Salvar()
 }
 
 function resetar() {
     n = 0
     cont.innerHTML = 0
+    Salvar()
+}
+
+function Salvar() {
+    localStorage.setItem("cont", cont.innerText)
 }
